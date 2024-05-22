ab6a0dedaa3a1a38d923fc5820314efea99e986b
cmrajendran <cmohan.biology@gmail.com>
2024-05-22 20:47:04 +0800

    Update app1.py

--- app1.py
+++ app1.py
@@ -12,24 +12,6 @@ st.markdown("""
 Welcome to the Health Claim Fraud Predictor! This tool helps in identifying potential fraudulent healthcare claims based on provider and claim information.
 Please fill out the details below to get started.
 """)
-# Add a relevant title image using HTML for full-width display
-st.markdown(
-    
-    <style>
-    .title-image {
-        width: 100%;
-        height: auto;
-    }
-    </style>
-    ,
-    unsafe_allow_html=True
-)
-
-st.markdown(
-    
-    <a href="https://imgbb.com/"><img src="https://i.ibb.co/Xj3ryjM/Untitled-design.jpg" alt="Untitled-design" border="0"></a>
-    ,
-    unsafe_allow_html=True
 
 @st.cache_data
 def preprocess_data(df):
